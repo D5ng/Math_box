391f179dde98c48db180134d4cdd97daaa5377e2
D5ng <121039744+D5ng@users.noreply.github.com>
2024-01-07 12:31:51 +0900

    Add say_hello function

--- calculator.js
+++ calculator.js
@@ -2,3 +2,5 @@ const addNumber = (x, y) => x + y
 const subtract = (x, y) => x - y
 const multiply = (x, y) => x * y
 const divide_premium = (x, y) => x / y;
+
+const say_hello = () => console.log("hello")
