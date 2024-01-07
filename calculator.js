5cf6d4a2a6c14274507b3efdec287e10ee3f264c
d5ng <dong5ffice@gmail.com>
2024-01-07 13:37:42 +0900

    Revert "Add square function"
    This reverts commit 8cc305f3547706b268248442d3738355a96d586c.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 const addNumber = (x, y) => x + y
 const subtract = (x, y) => x - y
 const multiply = (x, y) => x * y
-const divide_premium = (x, y) => x / y;
-const square = (x) => x * x
+const divide_free = (x, y) => x / y;
+
