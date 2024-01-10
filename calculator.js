9e1244f33d936c544d1dcf95378329888ccf95ac
d5ng <dong5ffice@gmail.com>
2024-01-10 23:07:38 +0900

    .

--- calculator.js
+++ calculator.js
@@ -2,4 +2,3 @@ const addNumber = (x, y) => x + y
 const subtract = (x, y) => x - y
 const multiply = (x, y) => x * y
 const divide_premium = (x, y) => x / y;
-const getMedian = (x, y) => (x + y) / 2
