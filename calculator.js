e51dc793862652d5983cc86c426d73bceb11fb6c
d5ng <dong5ffice@gmail.com>
2024-01-10 23:06:55 +0900

    Add getRemainder function

--- calculator.js
+++ calculator.js
@@ -2,4 +2,4 @@ const addNumber = (x, y) => x + y
 const subtract = (x, y) => x - y
 const multiply = (x, y) => x * y
 const divide_premium = (x, y) => x / y;
-const getMedian = (x, y) => (x + y) / 2
+const getRemainder = (x, y) => x % y;
